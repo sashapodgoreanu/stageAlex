eea0a971ac9add82a7fe626321d66d0cb24abc59
a.podgoreanu <a.podgoreanu@apodgoreanu.replynet.prv>
2015-05-18 10:47:49 +0200

    Merge origin/master
    Conflicts:
    	src/main/webapp/WEB-INF/jsp/template/head.jsp

--- src/main/java/com/unito/controller/ControllerHome.java
+++ src/main/java/com/unito/controller/ControllerHome.java
@@ -5,6 +5,15 @@
  */
 package com.unito.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,26 +25,44 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class ControllerHome {
-    
+
+    @Autowired
+    public AuthenticationProvider authenticationManager;
+
+
     @RequestMapping(value = "helloWorld", method = RequestMethod.GET)
-    public ModelAndView thiWillNotBeHello(){
+    public ModelAndView thiWillNotBeHello() {
         ModelAndView mv = new ModelAndView("displayName");
-        mv.addObject("hello","Hello");
+        mv.addObject("hello", "Hello");
         return mv;
     }
-    
+
     /*@RequestMapping(value = {"/login","/"}, method = RequestMethod.GET)
-    public String indexR(){
-        return "redirect: index";
-    }*/
-    
-    @RequestMapping(value = {"/index","/login"}, method = RequestMethod.GET)
-    public String index(){
+     public String indexR(){
+     return "redirect: index";
+     }*/
+    @RequestMapping(value = {"/index", "/login"}, method = RequestMethod.GET)
+    public String index(HttpServletRequest request, @AuthenticationPrincipal Object customUser) {
+        System.out.println(customUser.toString());
         return "index";
     }
-    
+
+    public void login(HttpServletRequest request, String userName, String password) {
+
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(userName, password);
+
+        // Authenticate the user
+        Authentication authentication = authenticationManager.authenticate(authRequest);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+
+        // Create a new session and add the security context.
+        HttpSession session = request.getSession(true);
+        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+    }
+
     @RequestMapping(value = "/workingarea", method = RequestMethod.GET)
-    public String working_area(){
+    public String working_area() {
         return "working_area";
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -32,25 +32,25 @@
                             <div class="form-group">
                                 <label for="inputUsername" class="col-sm-2 control-label">Username</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="inputUsername" placeholder="Pick a username">
+                                    <input type="text" name = "username" class="form-control" id="inputUsername" placeholder="Pick a username">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
                                 <div class="col-sm-10">
-                                    <input type="email" class="form-control" id="inputEmail3" placeholder="Log in">
+                                    <input type="email" name  ="email" class="form-control" id="inputEmail3" placeholder="Log in">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
                                 <div class="col-sm-10">
-                                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                    <input type="password" name ="password" class="form-control" id="inputPassword3" placeholder="Password">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <div class="col-sm-offset-2 col-sm-10">
                                     <div class=" col-sm-5">
-                                        <button type="submit" class="btn btn-default">Sign up</button>
+                                        <button type="submit" class="btn btn-default">Login</button>
                                     </div>
                                     <div class="col-sm-5">
                                         <button type="submit" class="btn btn-default">Sign up</button>
